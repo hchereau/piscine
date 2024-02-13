4545df8a88fe5f85e8d3e77d1b997653dc9dbed6
oui <hchereau42@gmail.com>
2024-02-13 22:04:59 +0100

    finish

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,18 @@
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	size_t	i_dest;
+	size_t	i_src;
+
+	i_dest = 0;
+	i_src = 0;
+	while (dest[i_dest])
+		++i_dest;
+	while (i_src < n && src[i_src])
+	{
+		dest[i_dest] = src[i_src];
+		++i_dest;
+		++i_src;
+	}
+	dest[i_dest] = '\0';
+	return (i_dest);
+}
