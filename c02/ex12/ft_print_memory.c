5b1458356fc0611b733269e933306d0f73313eb8
Hugo Chereau <hucherea@made-f0Br4s6.clusters.42paris.fr>
2024-02-13 19:49:20 +0100

    try_fix_12

--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,14 +6,14 @@
 /*   By: hucherea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 18:54:30 by hucherea          #+#    #+#             */
-/*   Updated: 2024/02/13 18:25:35 by hucherea         ###   ########.fr       */
+/*   Updated: 2024/02/13 19:47:55 by hucherea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <unistd.h>
 
-#define BASE_HEXA "0123456789abcdef"
+#define HEXA_BASE "0123456789abcdef"
 /*
 void	print_address(unsigned long addr, const char *base)
 {
@@ -60,14 +60,14 @@ void	print_char_hexa(char *addr, const char *base, int	size)
 
 void	print_hexa(unsigned long value, int size_output)
 {
-	char		string_output[16];
-	const char	*hex_digit = HEXA_BASE;
+	char		string_output[16] = {'0'};
+	char		*hex_digit = HEXA_BASE;
 	size_t		i;
 
 	i = size_output;
 	while (i > 0)
 	{
-		string_ouput[i - 1] = hex_digit[value % 16];
+		string_output[i - 1] = hex_digit[value % 16];
 		value = value / 16;
 		--i;
 	}
@@ -102,31 +102,45 @@ void	put_spaces(int	size)
 	}
 }
 
+void	print_char_hexa(char *line, int nb)
+{
+	int	i;
+
+	i = 0;
+	while (i < nb)
+	{
+		print_hexa(*line, 2);
+		if (i % 2 == 1)
+			write(1, " ", 1);
+		++i;
+		++line;
+	}
+}
+
 void	*ft_print_memory(void *addr, unsigned int size)
 {
 	unsigned long	ad;
-	const char		base[] = "0123456789abcdef";
-	unsigned int	nb_bytes;
+	 int			nb_bytes;
 
 	nb_bytes = size;
 	ad = (unsigned long)addr;
 	while (nb_bytes > 0)
 	{
-		print_address(ad, base);
+		print_hexa(ad, 16);
 		if (nb_bytes > 16)
 		{
-			print_char_hexa(((char *)ad), base, 16);
+			print_char_hexa(((char *)ad), 16);
 			print_content(((char *)ad), 16);
 		}
 		else
 		{
-			print_char_hexa(((char *)ad), base, nb_bytes);
+			print_char_hexa(((char *)ad), nb_bytes);
 			put_spaces(16 - nb_bytes);
-			print_content(((char *)ad), nb_bytes);	
+			print_content(((char *)ad), nb_bytes);
 		}
 		write(1, "\n", 1);
 		ad += 16;
-		nb_bytes -= nb_bytes % 16;
+		nb_bytes -= 16;
 	}
 	return(addr);
 }
@@ -134,7 +148,7 @@ void	*ft_print_memory(void *addr, unsigned int size)
 int	main()
 {
 	char *str = "Bonjour les aminches...c. est fou\ntout\tce qu on9 peut faire avec\e ..print_memory\n.\nlol.lol\n \n";
-	ft_print_memory(str, 200);
+	ft_print_memory(str, 91);
 }
 /*
 addr = unsigned long
